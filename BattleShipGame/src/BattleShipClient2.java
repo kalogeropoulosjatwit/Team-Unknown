aba336fe17108780bb4a4fd4c99539723c744ce3
stevenst6atwit <71520627+stevenst6atwit@users.noreply.github.com>
2020-11-16 16:07:07 -0500

    Add files via upload

---
+++ BattleShipGame/src/BattleShipClient2.java
@@ -0,0 +1,28 @@
+
+/**
+ * 
+ */
+
+/**
+ * 
+ *
+ * @author tstev
+ * @version 1.0.0 2020-11-16 Initial implementation
+ *
+ */
+public class BattleShipClient2
+    {
+
+    /**
+     * 
+     *
+     * @param args
+     */
+    public static void main( String[] args )
+        {
+        // TODO Auto-generated method stub
+
+        }
+
+    }
+	// end class BattleShipClient2
