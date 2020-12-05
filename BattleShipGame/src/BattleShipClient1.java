57dbfb8e723cfa3daee6802e44dafd2fd35ac2d6
tstev <tstev@MSI.home>
2020-12-05 16:01:12 -0500

    12/5/2020 4:00

--- BattleShipGame/src/BattleShipClient1.java
+++ BattleShipGame/src/BattleShipClient1.java
@@ -62,31 +62,49 @@ public class BattleShipClient1
 
 
         /**
-         * 
-         *
-         * @return
          */
-        public static String getLocations() {
+        public static void getLocations() {
         Scanner s = new Scanner (System.in);
-        String location;
-        char r,c;
-        boolean valid;
+        String destroyer; //2
+        String submarine; //3
+        String battleShip; //4
+        String aircraftCarrier; //5
+       
         
-        System.out.println("Enter Location... Column: a-j, Row: 0-9 (Ex:a9)");
-        location = s.next();
+        System.out.println("Enter Location for destroyer (2 spots) Column: a-j, Row: 0-9 (Ex:a8a9)");
+        destroyer = s.next();
+        if(destroyer.length() == 4 ) {
+        String dSpot1 = destroyer.substring(0,2);
+        String dSpot2 = destroyer.substring( 2,4 );
+        } 
+        System.out.println("Enter Location for destroyer (3 spots) Column: a-j, Row: 0-9 (Ex:b7b8b9)");
+        submarine = s.next();
+        if(submarine.length() == 6 ) {
+        String sSpot1 = submarine.substring(0,2);
+        String sSpot2 = submarine.substring( 2,4 );
+        String sSpot3 = submarine.substring( 4,6 );
+        } 
+        System.out.println("Enter Location for destroyer (4 spots) Column: a-j, Row: 0-9 (Ex:c6c7c8c9)");
+        battleShip = s.next();
+        if(battleShip.length() == 8) {
+        String bSpot1 = battleShip.substring(0,2);
+        String bSpot2 = battleShip.substring( 2,4 );
+        String bSpot3 = battleShip.substring( 4,6 );
+        String bSpot4 = battleShip.substring( 6,8 );
+        } 
+        System.out.println("Enter Location for destroyer (5 spots) Column: a-j, Row: 0-9 (Ex:d5d6d7d8d9)");
+        aircraftCarrier = s.next();
+        if(aircraftCarrier.length() == 10) {
+        String aSpot1 = aircraftCarrier.substring(0,2);
+        String aSpot2 = aircraftCarrier.substring( 2,4 );
+        String aSpot3 = aircraftCarrier.substring( 4,6 );
+        String aSpot4 = aircraftCarrier.substring( 6,8 );
+        String aSpot5 = aircraftCarrier.substring( 8,10 );
+        } 
         
-            if(location.length() == 2 && r>= 'a' && r<= 'j' && c >= '0' && c>= '9') {
-            valid = true;
-            r = location.charAt( 0 );
-            c = location.charAt(1);
-            }
-                else {
-                valid = false;
-                }
-                    if(!valid) {
-                    System.out.println("Invalid Location");
-                    }
-        return location;
+       
+            
+                
         }
                     
         }    
